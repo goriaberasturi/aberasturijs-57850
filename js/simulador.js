39a380a93528f0435296f1c5080f6a72ad7bab08
gregorio aberasturi <gregorioaberasturi@gmail.com>
2023-07-16 21:08:47 -0300

    Terminada pre-entrega 2

--- js/simulador.js
+++ js/simulador.js
@@ -1,23 +1,59 @@
-const calculos = ['Sumar', 'Restar', 'Multiplicar', 'Dividir'];
+const calculos = ['Sumar', 'Restar', 'Multiplicar', 'Dividir', 'Iterar suma'];
 let calcEntry;
-let num1;
-let num2;
-let resultado;
+let resultado = 0;
+let numeros;
+let respuesta;
 
-function sumar(x, y) {
-    return x + y;
+function rango(inicio, fin) {
+    let array = [];
+
+    for(let i = inicio; i <= fin; i++) {
+        array.push(i);
+    }
+
+    return array;
 }
 
-function restar(x, y) {
-    return x - y;
-}
+class operar {
+    constructor(x, y) {
+        this.num1 = x;
+        this.num2 = y;
+    }
 
-function multiplicar(x, y) {
-    return x * y;
-}
+    sumar() {
+        return this.num1 + this.num2;
+    }
+    
+    restar() {
+        return this.num1 - this.num2;
+    }
+    
+    multiplicar() {
+        return this.num1 * this.num2;
+    }
+    
+    dividir() {
+        if(this.num2 == 0) {
+            alert('Error! La division por cero no esta definida');
+        } else {
+            return this.num1 / this.num2;
+        }
+    }
 
-function dividir(x, y) {
-    return x / y;
+    itSuma() {
+        if(this.num1 < this.num2) {
+            let rangoIterado = rango(this.num1, this.num2);
+
+            rangoIterado.forEach(item => {resultado += Number(item)});            
+        } else {
+
+            let rangoIterado = rango(this.num2, this.num1);
+            
+            rangoIterado.forEach(item => {resultado += item});
+        }
+
+        return resultado;
+    }
 }
 
 function preguntaOp() {
@@ -31,53 +67,49 @@ function preguntaOp() {
         num2 = Number(prompt('Introduzca el segundo numero:'));
     } while (isNaN(num2));
 
-    if (calcEntry == 0 || calcEntry == 1) {
-        alert(`A continuacion, el resultado de ${calculos[calcEntry].toLocaleLowerCase()} ${num1} y ${num2}`);
-    } else {
-        alert(`A continuacion, el resultado de ${calculos[calcEntry].toLocaleLowerCase()} ${num1} por ${num2}`);
-    }
+    numeros = new operar(num1, num2);
 }
 
 do {
-    calcEntry = Number(prompt('Elija un calculo a realizar:' +
-                            '\n[0] - ' + calculos[0] +
-                            '\n[1] - ' + calculos[1] +
-                            '\n[2] - ' + calculos[2] +
-                            '\n[3] - ' + calculos[3]
-                            )
-    );
-
-    if (calcEntry != 0 && calcEntry != 1 && calcEntry != 2 && calcEntry != 3) {
-        alert('La opcion seleccionada no existe');
-    }
-} while (calcEntry != 0 && calcEntry != 1 && calcEntry != 2 && calcEntry != 3);
-
-switch (calcEntry) {
-
-    case 0:
-        preguntaOp();
-        alert(`El resultado es:   ${num1 + num2}`);
-        break;
-
-    case 1:
-        preguntaOp();
-        alert(`El resultado es:   ${num1 - num2}`);
-        break;
-
-    case 2:
-        preguntaOp();
-        alert(`El resultado es:   ${num1 * num2}`);
-        break;
-
-    case 3:
-        preguntaOp();
-        if(num2 == 0) {
-            alert(`Error: se intento dividir por 0!`);
-        } else {
-            alert(`El resultado es:   ${num1 / num2}`);
+    do {
+        calcEntry = Number(prompt('Elija un calculo a realizar:' +
+                                '\n[0] - ' + calculos[0] +
+                                '\n[1] - ' + calculos[1] +
+                                '\n[2] - ' + calculos[2] +
+                                '\n[3] - ' + calculos[3] +
+                                '\n[4] - ' + calculos[4]
+                                )
+        );
+    
+        if (!rango(0, calculos.length-1).includes(calcEntry)) {
+            alert('La opcion seleccionada no existe');
         }
-        break;
+    } while (!rango(0, calculos.length-1).includes(calcEntry));
 
-    default:
-        alert('La opcion seleccionada no existe');
-}
+    preguntaOp();
+
+    switch(calcEntry) {
+        case 0:
+            alert('El resultado de la suma es: ' + numeros.sumar());
+            break;
+    
+        case 1:
+            alert('El resultado de la resta es: ' + numeros.restar());
+            break;
+    
+        case 2:
+            alert('El resultado de la multiplicacion es: ' + numeros.multiplicar());
+            break;
+    
+        case 3:
+            alert('El resultado de la division es: ' + numeros.dividir());
+            break;
+    
+        case 4:
+            alert('El resultado de iterar este rango es: ' + numeros.itSuma());
+            break;
+    }
+    
+    respuesta = confirm('Desea continuar con otro calculo?');
+} while(respuesta);
+
