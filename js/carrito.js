3ec3d2841834258915ef3cc25efab64790218aff
gregorio aberasturi <gregorioaberasturi@gmail.com>
2023-07-29 05:26:02 -0300

    Avanzada 3ra pre-entrega

---
+++ js/carrito.js
@@ -0,0 +1,40 @@
+let lotes = [];
+let carrito = [];
+let cardLinks = document.querySelectorAll('.cardLink');
+let clon;
+
+const container = document.querySelector('#container');
+const message = document.querySelector('.article2 h2');
+document.querySelector('#updateBtn').onclick = () => {actualizar()};
+
+function confirmar(lote) {
+    respuesta = confirm(`Desea agregar el lote ${lote} al carrito`);
+
+    respuesta && (carrito.indexOf(lote) != -1 ? alert('El lote ya se encuentra en el carrito!') :  carrito.push(lote));
+    console.log(carrito);
+}
+
+function actualizar() {
+    console.log(carrito.length);
+
+    if(carrito.length == 0) {
+        container.style.display = 'none';
+    } else {
+        message.style.display = 'none';
+        container.style.display = 'inline-block';
+
+        for(producto of carrito) {
+            console.log(lotes[producto]);
+            container.innerHTML += cardLinks[producto];
+        }
+    }
+}
+
+for(card of cardLinks) {
+    lotes.push(card);
+    let pos = lotes.indexOf(card) + 1;
+    card.onclick = () => confirmar(pos);
+    console.log(lotes);
+}
+
+cardLinks.forEach(item => item.classList.add(lotes.indexOf(item)+1));
