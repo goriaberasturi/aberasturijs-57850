43d4fbed2612fad18f283969d0ea54bed1ba4005
gregorio aberasturi <gregorioaberasturi@gmail.com>
2023-08-15 20:14:56 -0300

    Se agrego y personalizo sweet alert

--- js/cart.js
+++ js/cart.js
@@ -3,10 +3,9 @@ fetch('productos.json')
     .then((resp) => resp.json())
     .then((prods) => {
         catalogo = prods;
-        for(let i = 0; i < catalogo.length; i++) {
+        for (let i = 0; i < catalogo.length; i++) {
             document.querySelector(`#l${i + 1}`).onclick = () => agregar(i);
         }
-        console.log(catalogo);
     });
 let cart = [];
 let cartLS = JSON.parse(localStorage.getItem('carritoLotes'));
@@ -14,40 +13,73 @@ let message = document.querySelector('#message');
 let container = document.querySelector('#container');
 
 document.querySelector('#clearBtn').addEventListener('click', function limpiar() {
-    cart = [];
-    localStorage.clear();
-    container.innerHTML = '';
-    message.style.display = 'block'
+    Swal.fire({
+        title: `Desea vaciar el carrito?`,
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: 'Vaciar',
+        confirmButtonColor: '#a5dc86',
+        cancelButtonText: 'Cancelar',
+        cancelButtonColor: '#f27474',
+
+    }).then((result) => {
+        if (result.isConfirmed) {
+            cart = [];
+            localStorage.clear();
+            container.innerHTML = '';
+            message.style.display = 'block';
+
+            Swal.fire({
+                title: 'El carrito fue vaciado!',
+                icon: 'success',
+                showConfirmButton: false,
+                timer: 1500
+            })
+        }
+    });
 });
 
 function actualizar() {
     cartLS = JSON.parse(localStorage.getItem('carritoLotes'));
     cartLS && (cart = cartLS);
-    
-    if(cart.length != 0) {
+
+    if (cart.length != 0) {
         message.style.display = 'none';
         container.style.display = 'grid'
         container.innerHTML = '';
-        for(product of cart) {
-            container.innerHTML += `<a class="cardLink" href="#"><div class="selfcard"><div class="cardImg l${product.id +1}"></div><div class="cardTxt"><h3 class="card-title">${product.title}</h3><p class="card-text">${product.description}</p></div></div></a>`;
+        for (product of cart) {
+            container.innerHTML += `<a class="cardLink" href="#"><div class="selfcard"><div class="cardImg l${product.id + 1}"></div><div class="cardTxt"><h3 class="card-title">${product.title}</h3><p class="card-text">${product.description}</p></div></div></a>`;
         }
     }
 }
 
 function agregar(element) {
     console.log('======= Inicio de la funcion agregar() =======');
-    respuesta = confirm(`Desea agregar el lote ${element + 1} al carrito?`);
-    
-    if(respuesta) {
-        if((cart.find(({id}) => id == catalogo[element].id)) == undefined) {
-            cart.push(catalogo[element]);
-            localStorage.setItem('carritoLotes', JSON.stringify(cart));
-            actualizar();
-            
-        } else {
-            alert('El lote ya se encuentra en el carrito!');
+    Swal.fire({
+        title: `Desea agregar el Lote ${element + 1} al carrito?`,
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: 'Agregar',
+        confirmButtonColor: '#a5dc86',
+        cancelButtonText: 'Cancelar',
+        cancelButtonColor: '#f27474',
+    }).then((result) => {
+
+        if (result.isConfirmed) {
+            if ((cart.find(({ id }) => id == catalogo[element].id)) == undefined) {
+                cart.push(catalogo[element]);
+                localStorage.setItem('carritoLotes', JSON.stringify(cart));
+                actualizar();
+            } else {
+                Swal.fire({
+                    title: 'El Lote ya se encuentra en el carrito!',
+                    icon: 'error',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+            }
         }
-    }
+    })
 }
 
 actualizar();
